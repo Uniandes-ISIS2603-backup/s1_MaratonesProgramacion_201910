f6c6e8da3c00cd7357162635a501681729ac9db7
estudiante <estudiante@ISIS2603-423.sis.virtual.uniandes.edu.co>
2019-02-12 17:16:04 -0500

    Merge origin/master
    Conflicts:
    	s1_maratones-back/src/main/java/co/edu/uniandes/csw/maratones/entities/UsuarioEntity.java

---
+++ s1_maratones-back/src/main/java/co/edu/uniandes/csw/maratones/entities/UsuarioEntity.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.maratones.entities;
+
+/**
+ *
+ * @author Angel Rodriguez aa.rodriguezv
+ */
+public class UsuarioEntity {
+     private String rol;
+    private String nombreUsuario;
+    private String nombre;
+    private String imagen;
+    private String correo;
+    private String clave;
+    private int puntaje;
+    
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+}
