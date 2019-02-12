5f2051ca253a24dda560ff61e66637a499c2a1c1
estudiante <estudiante@ISIS2603-423.sis.virtual.uniandes.edu.co>
2019-02-12 18:17:02 -0500

    Implementacion del test ejercicio y lenguaje

---
+++ s1_maratones-back/src/test/java/co/edu/uniandes/csw/maratones/test/persistence/EjercicioPersistenceTest.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.maratones.test.persistence;
+
+import co.edu.uniandes.csw.maratones.entities.EjercicioEntity;
+import co.edu.uniandes.csw.maratones.persistence.EjercicioPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author Angel Rodriguez aa.rodriguezv
+ */
+@RunWith(Arquillian.class)
+public class EjercicioPersistenceTest {
+    
+     
+    @Inject
+    private EjercicioPersistence ejercicioPersistence;
+    
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    
+    /**
+     * Lista que tiene los datos de prueba.
+     */
+    private List<EjercicioEntity> data = new ArrayList<EjercicioEntity>();
+    
+    
+    /**
+     * Variable para martcar las transacciones del em anterior cuando se
+     * crean/borran datos para las pruebas.
+     */
+    @Inject
+    UserTransaction utx;
+
+    
+     /**
+     *
+     * @return Devuelve el jar que Arquillian va a desplegar en el Glassfish
+     * embebido. El jar contiene las clases de Editorial, el descriptor de la
+     * base de datos y el archivo beans.xml para resolver la inyección de
+     * dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(EjercicioEntity.class.getPackage())
+                .addPackage(EjercicioPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    
+       /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+       /**
+     * Limpia las tablas que están implicadas en la prueba.
+     *
+     *
+     */
+    private void clearData() {
+        em.createQuery("delete from EjercicioEntity").executeUpdate();
+    }
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     *
+     *
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+
+            EjercicioEntity entity = factory.manufacturePojo(EjercicioEntity.class);
+
+            em.persist(entity);
+
+            data.add(entity);
+        }
+    }
+    
+    
+    
+    
+    
+    
+    public void createSubmissionTest()
+    {
+        EjercicioEntity sub = new EjercicioEntity();
+        em.persist(sub);
+        Assert.assertNotNull(sub.getId());
+    }
+    
+            
+    
+}
