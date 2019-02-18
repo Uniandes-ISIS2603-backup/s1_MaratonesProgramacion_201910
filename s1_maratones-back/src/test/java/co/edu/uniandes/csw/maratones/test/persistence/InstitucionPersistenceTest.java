cf2171ecf122c4d837d649dfc84dee268211a502
c.mendez11 <c.mendez11@uniandes.edu.co>
2019-02-17 20:13:32 -0500

    Adecuacion de Test's para Jenkins.

--- s1_maratones-back/src/test/java/co/edu/uniandes/csw/maratones/test/persistence/InstitucionPersistenceTest.java
+++ s1_maratones-back/src/test/java/co/edu/uniandes/csw/maratones/test/persistence/InstitucionPersistenceTest.java
@@ -6,14 +6,18 @@
 package co.edu.uniandes.csw.maratones.test.persistence;
 import co.edu.uniandes.csw.maratones.entities.InstitucionEntity;
 import co.edu.uniandes.csw.maratones.persistence.InstitucionPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -25,6 +29,17 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  */
 @RunWith(Arquillian.class)
 public class InstitucionPersistenceTest {
+    /**
+     * Lista que tiene los datos de prueba.
+     */
+    private List<InstitucionEntity> data = new ArrayList<InstitucionEntity>();
+    /**
+     * Variable para martcar las transacciones del em anterior cuando se
+     * crean/borran datos para las pruebas.
+     */
+    @Inject
+    UserTransaction utx;
+    
     @PersistenceContext
     private EntityManager em; 
     @Inject
@@ -37,6 +52,54 @@ public class InstitucionPersistenceTest {
                 .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml");
     }
+    
+     /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     *
+     *
+     */
+    private void clearData() {
+        em.createQuery("delete from InstitucionEntity").executeUpdate();
+    }
+
+     /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     *
+     *
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+
+            InstitucionEntity entity = factory.manufacturePojo(InstitucionEntity.class);
+
+            em.persist(entity);
+
+            data.add(entity);
+        }
+    }
+    
     @Test
     public void createInstitucionTest() {
         PodamFactory factory = new PodamFactoryImpl();
