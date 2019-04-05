0111327e66d861b93159847809d16c792ecdce73
estudiante <estudiante@ISIS2603-423.sis.virtual.uniandes.edu.co>
2019-04-05 15:00:20 -0500

    correccion ejercicio

--- s1_maratones-back/src/main/java/co/edu/uniandes/csw/maratones/entities/EjercicioEntity.java
+++ s1_maratones-back/src/main/java/co/edu/uniandes/csw/maratones/entities/EjercicioEntity.java
@@ -56,7 +56,7 @@ public class EjercicioEntity extends BaseEntity implements Serializable{
     private List<CompetenciaEntity> competencias;
     
     @PodamExclude
-    @OneToMany(mappedBy = "ejercicioEntitys",cascade = {
+    @OneToMany(mappedBy = "ejercicioEntity",cascade = {
         CascadeType.PERSIST,
         CascadeType.MERGE
     })
