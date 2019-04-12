ce47110707d0940c5267aab582cd71e06265749b
Cristian Mendez <c.mendez11@uniandes.edu.co>
2019-04-11 23:18:04 -0500

    Merge origin/mc.londono into c.mendez11
    Conflicts:
    	s1_maratones-api/collections/BlogResource-collection.postman_collection.json
    	s1_maratones-api/src/main/java/co/edu/uniandes/csw/maratones/dtos/EjercicioDTO.java
    	s1_maratones-api/src/main/java/co/edu/uniandes/csw/maratones/dtos/LenguajeDTO.java
    	s1_maratones-api/src/main/java/co/edu/uniandes/csw/maratones/dtos/PublicacionDTO.java
    	s1_maratones-api/src/main/java/co/edu/uniandes/csw/maratones/dtos/SubmissionDTO.java
    	s1_maratones-api/src/main/java/co/edu/uniandes/csw/maratones/resources/BlogResource.java
    	s1_maratones-api/src/main/java/co/edu/uniandes/csw/maratones/resources/PublicacionResource.java
    	s1_maratones-back/src/main/java/co/edu/uniandes/csw/maratones/entities/CompetenciaEntity.java

--- s1_maratones-api/src/test/java/co/edu/uniandes/csw/maratones/tests/postman/InstitucionIT.java
+++ s1_maratones-api/src/test/java/co/edu/uniandes/csw/maratones/tests/postman/InstitucionIT.java
@@ -53,7 +53,7 @@ public class InstitucionIT {
     @RunAsClient
     public void postman() throws IOException {
         PostmanTestBuilder tp = new PostmanTestBuilder();
-        tp.setTestWithoutLogin(COLLECTION, "Entorno-Colecciones.postman_environment");
+        tp.setTestWithoutLogin(COLLECTION, "Entorno-IT.postman_environment");
         String desiredResult = "0";
         Assert.assertEquals("Error en Iterations de: " + COLLECTION, desiredResult, tp.getIterations_failed());
 
